386ae5afc0869649cc928bb89ae9d5fbc545f0ba
at1234an <129424790+at1234an@users.noreply.github.com>
2023-03-30 17:58:57 +0000

    RENAMED AT

--- src/app/globals.js
+++ src/app/globals.js
@@ -1,2 +1,2 @@
-export const MY_NAME = "Bobby Connolly" // put your name here
-export const MY_EMAIL = "bobbyconnolly@gmail.com" // put your email here
+export const MY_NAME = "anbab" // put your name here
+export const MY_EMAIL = "at1234an.com" // put your email here
